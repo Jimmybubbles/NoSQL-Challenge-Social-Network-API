f4b846b0f7c14e2d157adc99a9461920ae1a89e9
james <james.anthony.russell36@gmail.com>
2022-12-14 19:06:54 +1100

    added models in mongoose form

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// require the user and thoughts Models
+const Thoughts = require('./thoughts')
+const Users = require('./users')
+
+// export the user and thoughts Models
+module.exports = { Users, Thoughts };
---
+++ models/users.js
@@ -0,0 +1,40 @@
+const { Schema, model} = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // use regex for the email validation.
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Users'
+    }]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getter: true,
+    },
+    id: false
+    }
+)
+
+// create the user model for the user schema
+const Users = model('Users', UserSchema);
+
+// Export Users module
+module.exports = Users;
